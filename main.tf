acdcbec26523d491dbc6b3642b7709b395109b91
indrajeet khandekar <indrajeet.khandekar@gmail.com>
2020-01-01 13:51:19 +0530

    server port is now a variable

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup python -m SimpleHTTPServer 8080 &
+                nohup python -m SimpleHTTPServer ${var.server_port} &
                 EOF
 
   tags = {
@@ -24,8 +24,8 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = var.server_port
+    to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -46,4 +46,10 @@ resource "aws_key_pair" "default" {
   key_name = "default-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSHarGPxDNLs8H7q+nsFZDMORhhR2eWzp/lVu4/EGX5nhl7CGiiIOY4VCX/v7y+VZFUtCZQAqCf2UgGIEpDyEWXmlXVt855tM7JprMPJ+txko6UtztL6p4Y4B8hkzaCEXOLLRKfW32nTNZK2LA4o8wKMS6Ppsf3xX3sCnWtjvIwZIZXnUKvhR0M8W35K59atGQ1ThAPVM/Vix0LtMePme60KldV9UukZBU283JQt6zfRNms370dcDQc6b23QuBH8hHtaZ8KMIUipEsz1RMzOtAHgxP/zG/G9j0l190h/ReNjEulel81uX3jsgLtLJdcw86fegM6Gsth+Xw2xW8B9Xz indrajeet@xps"
 
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
 }
